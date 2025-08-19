4e03ba15c14603a9c054dfeabe665d96baa85a45
Sviatoslav Tregubov <tregubov.sv@yandex.ru>
2025-08-19 21:13:37 +0300

    implement weighted round robin

--- internal/app/algorithms/weighted_round_robin.go
+++ internal/app/algorithms/weighted_round_robin.go
@@ -3,17 +3,13 @@ package algorithms
 import (
 	"load-balancer/internal/app/backend"
 	"sync"
+	"sync/atomic"
 )
 
 type WeightedRoundRobin struct {
-	pool  []*backend.Backend
-	mutex sync.RWMutex
-}
-
-func NewWeightedRoundRobin() *WeightedRoundRobin {
-	return &WeightedRoundRobin{
-		pool: make([]*backend.Backend, 0),
-	}
+	current   uint64
+	callCount uint
+	m         sync.Mutex
 }
 
 func (r *WeightedRoundRobin) Name() string {
@@ -21,48 +17,30 @@ func (r *WeightedRoundRobin) Name() string {
 }
 
 func (r *WeightedRoundRobin) GetNextBackend(backends []*backend.Backend) *backend.Backend {
-	/**
-	TODO возможно не нужно всей этой работы с добавлением/удалением бекендов.
-	Лучше при запуске создать сразу весь список бекендов и отправлять по ним так же как и в обычном RR алгоритме
-	*/
-
-	r.mutex.Lock()
 	backendsCount := len(backends)
 
 	if backendsCount == 0 {
 		return nil
 	}
 
-	if r.pool == nil || len(r.pool) == 0 {
-		r.pool = make([]*backend.Backend, 0)
-		for _, b := range backends {
-			for i := 0; i < int(b.Weight); i++ {
-				r.pool = append(r.pool, b)
-			}
+	r.m.Lock()
+	defer r.m.Unlock()
+
+	next := r.current
+	if r.callCount == 0 {
+		next = atomic.AddUint64(&r.current, uint64(1)) % uint64(backendsCount)
+		r.callCount = backends[int(next)].Weight
+	}
+
+	r.callCount--
+
+	for i := 0; i < backendsCount; i++ {
+		idx := (int(next) + i) % backendsCount
+
+		if backends[idx].IsAlive() {
+			return backends[idx]
 		}
 	}
 
-	var resultBackend *backend.Backend
-	for i, b := range r.pool {
-		if b.IsAlive() {
-			resultBackend = b
-			r.pool[i] = nil
-			break
-		}
-	}
-
-	filteredPool := make([]*backend.Backend, 0)
-	for _, b := range r.pool {
-		if b != nil {
-			filteredPool = append(filteredPool, b)
-		}
-	}
-	r.pool = filteredPool
-	r.mutex.Unlock()
-
-	return resultBackend
-}
-
-func (r *WeightedRoundRobin) removeBackendFromPool(backends []*backend.Backend, index int) []*backend.Backend {
-	return append(backends[:index], backends[index+1:]...)
+	return nil
 }
